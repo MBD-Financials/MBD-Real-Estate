068693333f3cec9c1d32c73328205bdd68a3603e
Captain F Harlock <info@mbdfinancials.com>
2023-01-31 21:38:44 +0100

    Eslint.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  
+  extends: [
+    //...
+    'plugin:@next/next/recommended',
+  ],
+  
   reactStrictMode: true,
   images:{
     domains:['bayut-production.s3.eu-central-1.amazonaws.com']
